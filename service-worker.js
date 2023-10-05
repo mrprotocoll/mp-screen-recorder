f369f57210b2389c11115be2d421958cc7c955a4
mrprotocoll <lekanvgbg@gmail.com>
2023-10-05 16:11:16 +0100

    start implementing recorded screen output

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
-//chrome
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
     if(changeInfo.status === "complete" && /^http/.test(tab.url)){
         chrome.scripting.executeScript({
             target: {tabId},
             files: ["./js/helpers.js","./js/content.js"]
         }).then(()=>{
-            console.log("we have injected the content script")
-        }).catch(err=> console.log(err, "error in background script line 10"))
+            console.log("scripts injected")
+        }).catch(err=> console.log(err, "couldn't inject script"))
     }
 })
