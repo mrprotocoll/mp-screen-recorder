88e9a06de871ee62b40ea70bc7cc66ea3a11f64b
mrprotocoll <lekanvgbg@gmail.com>
2023-10-09 12:25:13 +0100

    clean up files

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     // GET THE SELECTORS OF THE BUTTONS
     const startVideoButton = document.querySelector("button#start_video")
-    // const stopVideoButton = document.querySelector("button#stop_video")
 
     // adding event listeners
-
     startVideoButton.addEventListener("click", ()=>{
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {action: "request_recording"},  function(response){
@@ -16,19 +14,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
             })
         } )
     })
-
-
-    // stopVideoButton.addEventListener("click", ()=>{
-    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    //         chrome.tabs.sendMessage(tabs[0].id, {action: "stopvideo"},  function(response){
-    //             if(!chrome.runtime.lastError){
-    //                 console.log(response)
-    //             } else{
-    //                 console.log(chrome.runtime.lastError, 'error line 27')
-    //             }
-    //         })
-    //     } )
-    // })
 
 })
 
